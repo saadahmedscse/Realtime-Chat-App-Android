5c888a22e62bb72297019c0951914955181aa4b8
saadahmedscse <saadahmedscse@gmail.com>
2023-08-28 22:53:26 +0600

    created login response class

---
+++ app/src/main/java/com/saadahmedsoft/sparkconvo/service/dto/auth/LoginResponse.kt
@@ -0,0 +1,7 @@
+package com.saadahmedsoft.sparkconvo.service.dto.auth
+
+data class LoginResponse(
+    val status: Boolean? = null,
+    val message: String? = null,
+    val token: String? = null
+)
